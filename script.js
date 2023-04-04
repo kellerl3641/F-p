251bebb770925b820a0ab3e77a82b2fe4f25b73a
Keller Lin <114589962+kellerl3641@users.noreply.github.com>
2023-04-04 21:00:44 +0000

    Tue, Apr 4, 2023, 9:00 PM +00:00

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         var hash = this.hash;
 
         // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        // (900) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 900, function(){
